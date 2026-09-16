949f745cdf4d8560876756f41e0cde14bf51712d
Taufikhusain Sunasara <sunasarataufikhusain@gmail.com>
2026-09-16 23:15:02 +0530

    DAY-7-Add validation, show and delete feature for Reviews.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require("path");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema, reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -34,6 +34,26 @@ app.get("/", (req, res) => {
     res.send("working brooo!");
 })
 
+const validateListing = (req,res,next) => {
+    let { error } = listingSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+};
+
+const validateReview = (req,res,next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+};
+
 // Index route
 app.get("/listings", wrapAsync(async (req, res) => {
     let allListings = await Listing.find({});
@@ -49,14 +69,13 @@ app.get("/listings/new", (req, res) => {
 // Show route
 app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", { listing });
 }))
 
 // Create route
-app.post("/listings", wrapAsync(async (req, res) => {
+app.post("/listings", validateListing, wrapAsync(async (req, res) => {
     let result = listingSchema.validate(req.body);
-    console.log(result);
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -86,7 +105,9 @@ app.delete("/listings/:id",  wrapAsync(async (req, res) => {
 
 // Reviews
 // Post route
-app.post("/listings/:id/reviews", async (req,res) => {
+app.post("/listings/:id/reviews", 
+    validateReview, 
+    wrapAsync(async (req,res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
 
@@ -97,8 +118,17 @@ app.post("/listings/:id/reviews", async (req,res) => {
 
     console.log("New Review saved");
     res.redirect(`/listings/${listing._id}`);
-});
+}));
 
+// Review Delete Route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req,res) => {
+    let { id,reviewId } = req.params;
+    await Review.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+
+}))
 
 app.all(/(.*)/ , (req,res,next) => {
     next(new ExpressError(404, "page not found!!"));
--- schema.js
+++ schema.js
@@ -1,4 +1,5 @@
 const joi = require("joi");
+const review = require("./models/review");
 
 module.exports.listingSchema = joi.object({
     listing : joi.object({
@@ -10,3 +11,10 @@ module.exports.listingSchema = joi.object({
         image: joi.string().allow("",null)
     }).required()
 }) 
+
+module.exports.reviewSchema = joi.object({
+    review: joi.object({
+        rating: joi.number().required().min(1).max(5),
+        comment: joi.string().required(),
+    }).required()
+})
